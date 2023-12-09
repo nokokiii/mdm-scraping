978fa95716ae486ab303d0abbec90dbd86c1043e
nokokiii <kubanokoki@gmail>
2023-12-09 21:27:24 +0100

    update

--- inserting.py
+++ inserting.py
@@ -3,15 +3,12 @@ Automatically insert data into the database.
 """
 
 import json
+import logging
 import psycopg2
 
-DB_NAME = "db_name"
-USER = "username"
-PASSWORD = "password"
-HOST = "host"
-PORT = "port"
+DB_URL = ""
 
-CATEOGRIES_IDS = {
+CATEGORIES_IDS = {
     "carbonated-drinks": 1,
     "juices-and-fruit-drinks": 2,
     "sweets-and-chocolate": 3,
@@ -22,67 +19,66 @@ CATEOGRIES_IDS = {
 
 QUERY = "INSERT INTO products (name, image_link, ean, number_of_pieces, number_of_cases, number_of_pieces_on_pallet, is_packaged, is_palletized, category_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
 
-def get_data():
+logging.config(level=logging.INFO)
+
+def get_data() -> list[dict]:
     with open('./data.json', 'r') as f:
         data = json.load(f, strict=False)
     return data
 
 
-def check_product(checking_product: dict) -> bool:
-    if list(checking_product.keys()) == ["Name", "Category", "ImageLink", "EAN", "Number pieces in packaging", "Number of cases on the pallet", "Number of pieces on the pallet"] or list(product.keys()) == ["Name", "Category", "ImageLink", "EAN", "Number of pieces in the package", "Number of packages on a pallet", "Number of pieces on a pallet"]:
-        return True
-    else:
-        return False
+def is_product_correct(to_check: dict) -> bool:
+    return list(to_check.keys()) in (["Name", "Category", "ImageLink", "EAN", "Number pieces in packaging", "Number of cases on the pallet", "Number of pieces on the pallet"], ["Name", "Category", "ImageLink", "EAN", "Number of pieces in the package", "Number of packages on a pallet", "Number of pieces on a pallet"])
     
     
-def check_issued_data(issued_product: dict) -> bool:
-    required_keys = ["Name", "Category", "ImageLink"]
-    
-    for required_key in required_keys:
-        if required_key not in issued_product:
-            return False
-        
-    return True
+def is_issued(issued_product: dict) -> bool:
+    return any(key in issued_product for key in ["Name", "Category", "ImageLink"])
 
 
-def default_ean(checking_product: dict) -> str:
-    if "EAN" not in checking_product:
-        checking_product["EAN"] = None
-    return checking_product
+def set_ean(to_check: dict) -> str:
+    product = to_check.copy()
+    if "EAN" not in to_check:
+        product["EAN"] = None
+    return product
+
     
-def insert_data(data, is_issued=False):
-    product = default_ean(data)
-    
-    category_id = CATEOGRIES_IDS[product["Category"]]
+def insert_data(data: list[dict], is_issued: bool = False) -> None:
+    global cur
+    product = set_ean(data)
+    category_id = CATEGORIES_IDS[product["Category"]]
     
     if is_issued:
-        c.execute(QUERY, (product["Name"], product["ImageLink"], product["EAN"], None, None, None, False, False, category_id))
+        cur.execute(QUERY, (product["Name"], product["ImageLink"], product["EAN"], None, None, None, False, False, category_id))
     elif "Number pieces in packaging" in product:
-        c.execute(QUERY, (product["Name"], product["ImageLink"], product["EAN"], product["Number pieces in packaging"], product["Number of cases on the pallet"], product["Number of pieces on the pallet"], True, False, category_id))
+        cur.execute(QUERY, (product["Name"], product["ImageLink"], product["EAN"], product["Number pieces in packaging"], product["Number of cases on the pallet"], product["Number of pieces on the pallet"], True, False, category_id))
     elif "Number of pieces in the package" in product:
-        c.execute(QUERY, (product["Name"], product["ImageLink"], product["EAN"], product["Number of pieces in the package"], product["Number of packages on a pallet"], product["Number of pieces on a pallet"], True, False, category_id))
+        cur.execute(QUERY, (product["Name"], product["ImageLink"], product["EAN"], product["Number of pieces in the package"], product["Number of packages on a pallet"], product["Number of pieces on a pallet"], True, False, category_id))
     
+    
+def run():    
+    global cur
+    conn = psycopg2.connect(DB_URL)
+    cur = conn.cursor()
+    
+    data = get_data()
+    issued = []
+    
+    for product in data:
+        if is_product_correct(product):
+            insert_data(product, cur)
+        else:
+            issued.append(product)        
 
-try:
-    conn = psycopg2.connect(dbname=DB_NAME, user=USER, password=PASSWORD, host=HOST, port=PORT)
-    c = conn.cursor()
-except Exception as e:
-    print("Something went wrong with connection", e)
-    exit()
+    for issued_product in issued:
+        if is_issued(issued_product):
+            insert_data(issued_product, cur, is_issued=True)
+        else:
+            print(issued_product)
 
-data = get_data()
+    conn.commit()
+    cur.close()
+    conn.close()
 
-# List for products with issues
-issued = []
-
-for product in data:
-    if check_product(product):
-        insert_data(product)
-    else:
-        issued.append(product)        
-
-for issued_product in issued:
-    if check_issued_data(issued_product):
-        insert_data(issued_product, is_issued=True)
-    else:
-        print(issued_product)
+if __name__ == "__main__":
+    run()
+    
